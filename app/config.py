7a3e6f37ef0f1e7af4b111d0b7aabc3e876dcf97
Naveen <naveen98soni@gmail.com>
2026-06-20 03:17:10 +0100

    config file updated

--- app/config.py
+++ app/config.py
@@ -1,6 +1,9 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY", "")
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "AQ.Ab8RN6LxQjsalas52vBlkiatUOsdGDKcL0o3Fwmz6yp6Qzh7mA")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
 HOST = os.getenv("HOST", "127.0.0.1")
 PORT = int(os.getenv("PORT", "8000"))
